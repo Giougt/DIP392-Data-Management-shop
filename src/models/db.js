f1066d0e4197ea6d81786628244d0dcc2c8a10b7
Giougt <alexandre18golf@gmail.com>
2025-04-17 16:11:07 +0300

    update

--- src/models/db.js
+++ src/models/db.js
@@ -1,11 +1,11 @@
 const { Sequelize } = require("sequelize");
 
 /* const connection db */
-const NAME = process.env.DB_NAME || "BakeryManagement";
-const USER = process.env.DB_USER || "root";
-const PASSWORD = process.env.DB_PASSWORD || "Bonjour#Julien1";
+const NAME = process.env.DB_NAME || "name";
+const USER = process.env.DB_USER || "user";
+const PASSWORD = process.env.DB_PASSWORD || "password";
 const HOST = process.env.DB_HOST || "localhost";
-const PORT = process.env.DB_PORT || 3306; /*à127.0.0.1 */
+const PORT = process.env.DB_PORT || 1111; 
 
 const sequelize = new Sequelize(NAME, USER, PASSWORD, {
   host: HOST,
